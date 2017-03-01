d9867968e4e6b9aa159e71dbad76c39293ddbb8b
ChrisLeavitt <chris.leavitt2012@gmail.com>
2017-02-28 18:24:24 -0800

    Add files via upload

--- flags.vhd
+++ flags.vhd
@@ -1,35 +1,5 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/15/2017 09:19:49 AM
--- Design Name: 
--- Module Name: flags - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity flags is
     Port ( FLG_C_SET : in STD_LOGIC;
@@ -47,21 +17,39 @@ end flags;
 
 architecture Behavioral of flags is
 
+signal s_c, s_z, c_mux, z_mux, sh_c, sh_z : std_logic;
 begin
 
     process (CLK) begin 
         if rising_edge(CLK) then
             if FLG_C_LD = '1' then
-                C_FLAG_OUT <= C_FLAG_IN; 
+                s_c <= c_mux; 
             elsif FLG_C_SET = '1' then 
-                C_FLAG_OUT <= '1';
+                s_c <= '1';
             elsif FLG_C_CLR = '1' then
-                C_FLAG_OUT <= '0';
+                s_c <= '0';
             end if;
             if FLG_Z_LD = '1' then
-                Z_FLAG_OUT <= Z_FLAG_IN;
+                s_z <= z_mux;
+            end if;
+            if FLG_SHAD_LD = '1' then
+                sh_c <= s_c;
+                sh_z <= s_z;
             end if;
         end if;
+    end process;        
+            
+    process(FLG_LD_SEL) begin          
+      if FLG_LD_SEL = '1' then
+        c_mux <= sh_c;
+        z_mux <= sh_z;
+      else 
+        c_mux <= C_FLAG_IN;
+        z_mux <= Z_FLAG_IN;
+      end if;      
     end process;
+    
+    C_FLAG_OUT <= s_c;
+    Z_FLAG_OUT <= s_z;
      
 end Behavioral;
